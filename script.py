c5058015e599ffe7df4c675953ee6d75ed697bcb
MadGeckos2 <93497460+MadGeckos2@users.noreply.github.com>
2022-04-15 16:21:53 -0700

    Update script.py

--- script.py
+++ script.py
@@ -1,1 +1,15 @@
-print ("apple")
+import os
+os.system("pip install selenium==3.141.0")
+from webbot import Browser
+import time
+
+driver = Browser()
+
+time.sleep(5)
+driver.go_to('google.com')
+
+
+
+driver.new_tab(url='https://madgeckos.ml')
+
+input("Service is running")
